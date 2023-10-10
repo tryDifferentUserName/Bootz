7757458ec4f319cc4e7e66459e5fb7f10b3fe2a0
Erika Hvoinska <erika.hvoinska@accenture.com>
2023-10-10 17:24:50 +0300

    mobile nav implemented

--- src/components/TopNavigation.vue
+++ src/components/TopNavigation.vue
@@ -8,24 +8,36 @@ const menuLinks = ref(['Collection', 'Men', 'Women', 'About', 'Contact'])
 <template>
   <header>
     <div class="navigation-wrapper">
-      <div>
+      <div class="section-flex">
         <img src="@/assets/images/icon-menu.svg" alt="" v-if="isMobileView" />
         <RouterLink to="/"><img src="@/assets/images/logo.svg" alt="" /></RouterLink>
       </div>
       <nav v-if="!isMobileView">
         <RouterLink to="/" v-for="link in menuLinks" :key="link">{{ link }}</RouterLink>
       </nav>
-      <div>
+      <div class="section-flex">
         <RouterLink to="/cart"><img src="@/assets/images/icon-cart.svg" alt="" /></RouterLink>
-        <img src="@/assets/images/image-avatar.png" alt="" />
+        <img class="image-avatar" src="@/assets/images/image-avatar.png" alt="" />
       </div>
     </div>
   </header>
 </template>
 
 <style scoped>
+header {
+  width: 100%;
+  padding: 1.5rem;
+}
 .navigation-wrapper {
   display: flex;
   justify-content: space-between;
+}
+.section-flex {
+  display: flex;
+  gap: 1rem;
+}
+.image-avatar {
+  width: 1.5rem;
+  height: 1.5rem;
 }
 </style>
