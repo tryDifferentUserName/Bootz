68b7cff7eb5c706e5b120a83f920cb77c653e109
Erika Hvoinska <erika.hvoinska@accenture.com>
2023-10-10 17:11:09 +0300

    added assets

--- src/stores/products.js
+++ src/stores/products.js
@@ -5,9 +5,11 @@ export const useProductsStore = defineStore('products', () => {
   const products = ref([])
 
   async function fetchProducts() {
-    const response = await fetch('/assets/products.json')
+    console.log('Fetching products...')
+    const response = await fetch('@/assets/products.json')
     const productsData = await response.json()
     products.value = productsData
+    console.log(productsData)
   }
 
   return { products, fetchProducts }
