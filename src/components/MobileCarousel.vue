587bf1c4ade14c4bdcab3aa8c10e4e508d713c6e
Erika Hvoinska <erika.hvoinska@accenture.com>
2023-10-11 16:27:01 +0300

    wip carousel

---
+++ src/components/MobileCarousel.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps(['imageSrc'])
+</script>
+<template>
+  <img :src="imageSrc" />
+</template>
+<style scoped>
+img {
+  object-fit: cover;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,24 @@
-<script setup></script>
+<script setup>
+import boot1 from '@/assets/images/image-product-1.jpg'
+import boot2 from '@/assets/images/image-product-2.jpg'
+import boot3 from '@/assets/images/image-product-3.jpg'
+import boot4 from '@/assets/images/image-product-4.jpg'
+import MobileCarousel from '@/components/MobileCarousel.vue'
+
+const bootImages = [boot1, boot2, boot3, boot4]
+</script>
 
 <template>
-  <main></main>
+  <main>
+    <section class="carousel-image-wrapper">
+      <MobileCarousel :imageSrc="image" v-for="(image, index) in bootImages" :key="index" />
+    </section>
+  </main>
 </template>
+<style scoped>
+.carousel-image-wrapper {
+  display: flex;
+  overflow: hidden;
+  scroll-snap-type: x mandatory;
+}
+</style>
