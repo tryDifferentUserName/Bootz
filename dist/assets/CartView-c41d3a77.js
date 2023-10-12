33b05ef0b2b37d1d25c662dda31c30a52ca4ce01
Erika Hvoinska <erika.hvoinska@accenture.com>
2023-10-12 12:03:52 +0300

    Revert "dist build"
    This reverts commit 95f652a55e8172c3547d29ff53c74d74f5992502.

---
+++ dist/assets/CartView-c41d3a77.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c,a as o}from"./index-73d90668.js";const a={},s=o("h1",null,"cart page",-1),n=[s];function r(_,l){return t(),c("div",null,n)}const i=e(a,[["render",r]]);export{i as default};
